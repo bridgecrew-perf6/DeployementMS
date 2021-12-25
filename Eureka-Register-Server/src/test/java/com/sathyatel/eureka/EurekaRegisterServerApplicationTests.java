eba87789cf3fd7b3be10beecd886e6b710d42dc2
ngutt02 <nareshguttula1994@gmail.com>
2021-12-25 18:36:31 +0530

    first commit

---
+++ Eureka-Register-Server/src/test/java/com/sathyatel/eureka/EurekaRegisterServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sathyatel.eureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaRegisterServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
---
+++ Microservice-Friends/src/test/java/com/sathyatel/friend/MicroserviceFriendsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sathyatel.friend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+//naresh
+public class MicroserviceFriendsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
